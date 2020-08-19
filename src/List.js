0ee72528b8471fd63f4badc56f688eba6d5024e3
Cj <carterjpilling@gmail.com>
2020-08-19 09:54:24 -0600

    initial commit

---
+++ src/List.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class List extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      movies: []
+    }
+  }
+
+  
+  render() {
+    return (
+      <div>I am the list component</div>
+    )
+  }
+}
+
+export default List
